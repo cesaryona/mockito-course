0439ad51a31f2c2f95419a1bb23a625995ab5b3e
Cesar Yona <cesaryona_@outlook.com>
2021-04-03 13:27:05 -0300

    test 01

--- src/main/java/com/mockitotutorial/happyhotel/booking/BookingDAO.java
+++ src/main/java/com/mockitotutorial/happyhotel/booking/BookingDAO.java
@@ -1,10 +1,12 @@
 package com.mockitotutorial.happyhotel.booking;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class BookingDAO {
 
-	private final Map<String, BookingRequest> bookings = new HashMap<>();
+	private final Map<String, BookingRequest> bookings = new HashMap<String, BookingRequest>();
 
 	public String save(BookingRequest bookingRequest) {
 		String id = UUID.randomUUID().toString();
